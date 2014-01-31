beadc551a78a8b7ab0db3429c277587ae8fa12db
Marc-Alexandre Montpas <m4f10.s0h@gmail.com>
2014-01-30 19:26:00 -0500

    Fixing directory issues

---
+++ wpfuzz.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: WPFuzz
+ * Plugin URI: http://www.tmih-security.net/
+ * Description: Generic fuzzer backend needed to scan plugins.
+ * Version: 0.0.1
+ * Author: TMIH
+ * Author URI: http://www.tmih-security.net/
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+
+
+
+class WPFuzz {
+	function __construct() {
+		add_action('shutdown',array($this,'findHooks'));
+	}
+	
+	function findHooks() {
+		global $wp_filter;
+		return json_encode($wp_filter);
+		
+	}
+}
+
+$toto = new WPFuzz();
+
