b9f650fffd1b2b6f413e7151e7b9d1538306fee9
Alex Marquis <g4rg4n0th@gmail.com>
2014-02-13 20:40:30 -0500

    hooks.php completed

--- libs/hooks.php
+++ libs/hooks.php
@@ -9,10 +9,12 @@
 
 	class WPFuzzHooks {
 		private $defaultHooks; // Hooks and associated functions already used by WordPress
-		public	$currentHooks;
+		public	$currentHooks; 
 		
 		function __construct() {
 			$this->loadDefaultHooks();
+            		$this->loadCurrentHooks();
+            		$this->filterHooks();
 		}
 		
 		function loadDefaultHooks(){
@@ -30,8 +32,44 @@
 			global $wp_filter;
 			$this->currentHooks = json_encode( $wp_filter );
 		}
-		
-	}
-	
 
+		function filterHooks() {
+			$hooks = json_decode( $this->currentHooks, true );
+			$currenthooks_name = $this->fillArray($hooks);
+			$defaulthooks_name = $this->fillArray($this->defaulthooks);
+			$valid_hooks = array();							
+			
+			
+			foreach ( $currenthooks_name as $hook => $filter ) {
+				if (in_array( $hook, $defaulthooks_name ) === FALSE ) {
+					$valid_hooks[] = $hook;
+
+					foreach ( $filter as $key => $value ) {
+						$valid_hooks[$hook] = $key;					
+					}
+				}
+				else {
+					foreach ( $filter as $key => $value ) {
+						if ( in_array( $key, $defaulthooks_name[$hook] ) === FALSE ) {
+							$valid_hooks[] = $hook;
+							$valid_hooks[$hook] = $key;					
+						}
+					}
+				}							
+			}
+			return $valid_hooks;
+		}
+
+		function fillArray($hooks) {
+			$return_array = array();
+			foreach ( $hooks as $hook_name => $action ) {
+               			 $return_array[] = $hook_name;
+               			 foreach ( $action as $key => $value ) {
+					$return_array[$hook_name][] = $key;	
+                		 }
+			}
+
+			return $return_array;
+		}	
+	}
 ?>
